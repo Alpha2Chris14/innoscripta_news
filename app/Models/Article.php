4797efd3fdb25a7fbe996f82ccf252c9d46c9f67
Alpha2Chris14 <onyekachristian14@gmail.com>
2025-11-08 12:16:03 +0100

    added fillables and relationships for Soure and Aritcle

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    protected $fillable = [
+        'source_id',
+        'external_id',
+        'title',
+        'description',
+        'content',
+        'author',
+        'url',
+        'image_url',
+        'category',
+        'language',
+        'published_at',
+        'meta'
+    ];
+
+    protected $casts = ['meta' => 'array', 'published_at' => 'datetime'];
+
+    /**
+     * Get the source that owns the article. An article belongs to a source.
+     */
+    public function source()
+    {
+        return $this->belongsTo(Source::class);
+    }
 }
--- app/Models/Source.php
+++ app/Models/Source.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Source extends Model
 {
-    //
+    protected $fillable = ['name', 'slug', 'config', 'enabled'];
+    protected $casts = ['config' => 'array', 'enabled' => 'boolean'];
+
+    /**
+     * Get the articles for the source. A source can have many articles.
+     */
+    public function articles()
+    {
+        return $this->hasMany(Article::class);
+    }
 }
